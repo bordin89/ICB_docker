af812aec7124092470dbd2fa9fb6711b76918c9b
Nicola Bordin <bordin89@gmail.com>
2018-02-09 11:24:32 +0100

    Update make_tsv_table.py

--- make_tsv_table.py
+++ make_tsv_table.py
@@ -6,48 +6,22 @@ from collections import defaultdict
 
 Data=defaultdict(str)
 
+i=0
 infile=gzip.open( sys.argv[1] )
 for line in infile:
-    if line.startswith("NAME"):
-        continue
-    line=line.rstrip()
-    splitter=line.split("\t")
-    Data[splitter[0]]=splitter[1]+"\t"+splitter[2]+"\t"+splitter[3]
-
+	Data[i]=line.rstrip()
+	i+=1
 infile.close()
 
-infile=gzip.open(sys.argv[2])
-for line in infile:
-    check=0
-    line=line.rstrip()
-    if "Query" in line:
-        continue
-    else:
-        identifier=line.split("\t")[0]
-        info=line.replace(identifier,"")
-        for i in Data:
-            split_i=i.split("|")[1]
-            if split_i==identifier:
-                Data[i]+=info
-infile.close()
+for file in (sys.argv[2:]):
+	infile=gzip.open(file)
+	i=0
+	for line in infile:
+		info = line.split("\t",1)[1].rstrip()
+		info="".join(re.split("\s+", line.rstrip())[1:])
+		Data[i]+="\t"+info
+		i+=1
+	infile.close()
 
-for file in (sys.argv[3:]):
-    infile=gzip.open(file)
-    for line in infile:
-        line=line.rstrip()
-        if "Query" in line:
-            continue
-        else:
-            identifier=line.split("\t")[0]
-            info=line.replace(identifier,"")
-            identifier_pipe=identifier.split("|")[1]
-            for i in Data:
-                split_i=i.split("|")[1]
-                if split_i==identifier_pipe:
-                    Data[i]+=info
-    infile.close()
-
-print "NAME/ID\tDescription\tOrganism\tProtein length (aa)\t Best PSIBLAST Hit: UniProt Accession [+stats]\tBest PSIBLAST Hit: Gene\tBest PSIBLAST Hit: Description\t Best PSIBLAST Hit: GO Terms\tBest PSIBLAST Hit: Keywords\t Best PSIBLAST Hits: EC Number\tSignal Peptide\tTMHs\tDisorder % [Globular Domains]\tHHblits vs UniProt\tHHpred vs PDB"
 for i in Data:
-    print i+"\t"+Data[i]
-			
+	print Data[i]
